1be95c093a81c1645fdff1009e88051e668b1f25
Aleks <bla3e316@gmail.com>
2017-11-22 21:11:51 +0200

    Add Title Animation

--- Components/Scenes/Layouts/Elements/Title.js
+++ Components/Scenes/Layouts/Elements/Title.js
@@ -2,22 +2,43 @@ import React from 'react';
 import {
     Text,
     View,
+    Animated
 } from 'react-vr';
 
 //Element
 class Title extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            slideValue: new Animated.Value(1.5)};
+    }
+
+    componentDidMount() {
+        Animated.timing(
+            this.state.slideValue,
+            {
+                toValue: 0,
+                duration: 2000,
+                delay: 3000
+            }
+        ).start();
+    }
+
     render() {
         return (
             <View style={{ margin: 0.1, height: 0.5}}>
-                <Text
+                <Animated.Text
                     style={{
                         fontSize: 0.5,
                         fontWeight: '400',
                         textAlign: 'center',
-                        textAlignVertical: 'center'
+                        textAlignVertical: 'center',
+                        transform: [
+                            {scaleY: this.state.slideValue}
+                        ]
                     }}>
                     {this.props.text}
-                </Text>
+                </Animated.Text>
             </View>
         )
     }
