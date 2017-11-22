8e726e162aa0e7518c93148c61c16207270227f8
Aleks <bla3e316@gmail.com>
2017-11-22 12:05:32 +0200

    add 3dSound VideoPano

--- index.vr.js
+++ index.vr.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import MainMenu from './Components/Scenes/MainMenu';
 import {
-  AppRegistry,
-  asset,
-  Pano,
-  Text,
-  View,
+    AppRegistry,
+    asset,
+    Pano,
+    Text,
+    View,
+    Sound,
+    VideoPano
 } from 'react-vr';
 
 export default class VRTheater extends React.Component {
   render() {
     return (
       <View>
-        <Pano source={asset('night.jpg')}/>
+        <Pano source={asset('night.jpg')}>
+          <VideoPano
+          source ={
+            asset('',
+                {format: 'mp4', layout: 'SPHERE'}
+                )
+            }
+          >
+          <Sound
+            volume={0.8}
+            loop = {true}
+            source = {{mp3: asset('forest3d.mp3')}}
+          />
+          </VideoPano>
+        </Pano>
         <MainMenu/>
       </View>
     );
